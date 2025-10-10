5988978a01db5ff9496725d5105fe5c306c09516
aagusti <aagusti@nexusgeographics.com>
2025-10-10 10:13:34 +0200

    fix: remove development URLs

--- bimrocket-webapp/src/main/webapp/js/Environment.js
+++ bimrocket-webapp/src/main/webapp/js/Environment.js
@@ -2,9 +2,7 @@
 
 export const Environment =
 {
-  // TODO: Canviar SERVER_URL
-  // SERVER_URL : "/bimrocket-server",
-  SERVER_URL : "https://localhost:9090/bimrocket-server",
+  SERVER_URL : "/bimrocket-server",
   SERVER_ALIAS : "bimrocket",
   MODULES : ["base", "bim", "gis"]
 };
