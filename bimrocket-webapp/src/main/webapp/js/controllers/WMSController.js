e2a4c11f9964c908c3fd03d1935e9cb3a8f023ad
aagusti <aagusti@nexusgeographics.com>
2025-10-14 12:06:12 +0200

    feat: add wms controller

--- bimrocket-webapp/src/main/webapp/js/controllers/WMSController.js
+++ bimrocket-webapp/src/main/webapp/js/controllers/WMSController.js
@@ -1,124 +1,156 @@
-// /**
-//  * WMSController.js
-//  *
-//  * @author nexus
-//  */
+/**
+ * WMSController.js
+ *
+ * @author nexus, realor
+ */
 
-// import { Controller } from "./Controller.js";
-// import { WMSLoader } from "../io/gis/WMSLoader.js";
-// import { MapView } from "geo-three";
-// import * as THREE from "three";
-// import { MessageDialog } from "../ui/MessageDialog.js";
-// import proj4 from 'proj4';
+import { Controller } from "./Controller.js";
+import { MessageDialog } from "../ui/MessageDialog.js";
+import { WMSLoader } from "../io/gis/WMSLoader.js";
+import * as THREE from "three";
+import { MapView } from "geo-three";
+import proj4 from 'proj4';
 
-// proj4.defs("EPSG:3857", "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs");
-// proj4.defs("EPSG:25831", "+proj=utm +zone=31 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs");
+// Assegurem que les projeccions estiguin definides
+if (!proj4.defs["EPSG:3857"]) {
+  proj4.defs("EPSG:3857", "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs");
+}
+if (!proj4.defs["EPSG:25831"]) {
+  proj4.defs("EPSG:25831", "+proj=utm +zone=31 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs");
+}
 
-// class WMSController extends Controller
-// {
-//   static MAP_VIEW_NAME = "_wms_map_view";
+class WMSController extends Controller
+{
+  // static MAP_VIEW_NAME = "_wms_map_view";
 
-//   constructor(object, name)
-//   {
-//     super(object, name);
+  constructor(object, name)
+  {
+    super(object, name);
+    this.url = "";
+    this.layers = "";
+    this.crs = "EPSG:3857";
 
-//     this.url = ""; 
-//     this.layers = "";
-//     this.crs = "";
-//     this.origin = new THREE.Vector3(0, 0, 0);
+    this._mapView = null;
+    this._onNodeChanged = this.onNodeChanged.bind(this);
+    // this._animate = this.animate.bind(this);
 
-//     this.autoStart = true;
+    this._lastUrl = null;
+    this._lastLayers = null;
+    this._lastCrs = null;
+    this._lastOrigin = new THREE.Vector2();
 
-//     this._mapView = null;
-//     this._onNodeChanged = this.onNodeChanged.bind(this);
-//   }
+    this.autoStart = true;
+    this._isAnimating = false;
+  }
 
-//   onStart()
-//   {
-//     console.log("[WMSController] El mètode onStart() s'ha executat. El controlador està actiu.");
-//     this.application.addEventListener("scene", this._onNodeChanged);
-//     this.updateMap();
-//   }
+  onStart()
+  {
+    this.application.addEventListener("scene", this._onNodeChanged);
+    this.updateMap();
+  }
 
-//   onStop()
-//   {
-//     this.application.removeEventListener("scene", this._onNodeChanged);
-//     this.removeMap();
-//   }
+  onStop()
+  {
+    this.application.removeEventListener("scene", this._onNodeChanged);
+    this.stopAnimation();
+    this.removeMap();
+  }
 
-//   onNodeChanged(event)
-//   {
-//     if (event.type === "nodeChanged" && this.hasChanged(event))
-//     {
-//       console.log("[WMSController] Propietats canviades, actualitzant mapa...");
-//       this.updateMap();
-//     }
-//   }
+  onNodeChanged(event)
+  {
+    if (event.type === "nodeChanged" && this.hasChanged(event))
+    {
+      if (this.url !== this._lastUrl || 
+          this.layers !== this._lastLayers || 
+          this.crs !== this._lastCrs)
+          {
+        this.updateMap();
+      }
+    }
+  }
 
-//   updateMap()
-//   {
-//     this.removeMap();
+  updateMap()
+  {
+    this.removeMap();
 
-//     if (!this.url || !this.layers || !this.crs) {
-//       console.warn("WMSController: Falten paràmetres (URL, layers, CRS) per crear el mapa.");
-//       return;
-//     }
-    
-//     if (this.crs.toUpperCase() !== "EPSG:3857") {
-//         console.error("WMSController: Només es suporta CRS EPSG:3857.");
-//         return;
-//     }
+    if (!this.url || !this.layers || !this.crs) return;
+    if (this.crs.toUpperCase() !== "EPSG:3857")
+    {
+        MessageDialog.create("ERROR", `WMSController: CRS must be EPSG:3857, but got ${this.crs}.`).setI18N(this.application.i18n).show();
+        return;
+    }
 
-//     try
-//     {
-//       const application = this.application;
-//       const camera = application.camera;
+    this._lastUrl = this.url;
+    this._lastLayers = this.layers;
+    this._lastCrs = this.crs;
 
-//       const provider = new WMSLoader(this.url, this.layers, this.crs);
-//       const mapView = new MapView(MapView.PLANAR, provider, camera);
+    try
+    {
+      const application = this.application;
+      const camera = application.camera;
+      const provider = new WMSLoader(this.url, this.layers, this.crs);
+      const renderer = new THREE.WebGLRenderer();
+      const mapViewGeoT = new MapView(MapView.PLANAR, provider, camera);
       
-//       mapView.name = WMSController.MAP_VIEW_NAME;
-//       this._mapView = mapView; 
+      provider.minZoom = 13;
+      camera.position.z += 0.00001;
+      mapViewGeoT.name = "MapView";
+      mapViewGeoT.subDivisionsRays = 64;
+      renderer.render(application.scene, camera);
 
-//       mapView.position.sub(this.origin);
-//       mapView.updateMatrix();
-
-//       this.object.add(mapView);
+      function animate() 
+      {
+          requestAnimationFrame(animate);
+          mapViewGeoT.updateMatrixWorld(camera);
+          renderer.render(application.scene, camera);
+      }
+            
+      animate();
       
-//       application.notifyObjectsChanged(this.object, this, "structureChanged");
-//       console.log(`[WMSController] Capa WMS '${this.layers}' creada i afegida a l'objecte '${this.object.name}'.`);
-      
-//       application.repaint(); 
+      this._mapView = mapViewGeoT;
 
-//     } 
-//     catch (err)
-//     {
-//       console.error("Error durant la importació WMS:", err);
-//       MessageDialog.create("ERROR", "Error durant la importació: " + err.message)
-//         .setClassName("error")
-//         .setI18N(this.application.i18n).show();
-//     }
-//   }
+      this.object.add(this._mapView);
 
-//   removeMap()
-//   {
-//     if (this._mapView)
-//     {
-//       if (this._mapView.dispose) {
-//         this._mapView.dispose();
-//       }
-//       this._mapView.removeFromParent(); 
-//       this._mapView = null;
-//       this.application.notifyObjectsChanged(this.object, this, "structureChanged");
-//     }
-//   }
+      this.object.rotation.x = Math.PI/2;
+      this.object.position.set
+      (
+          253,
+          5668,
+          -1.5
+      );
+      this.object.updateMatrix();
+      this.object.updateMatrixWorld(true);
 
-//   static getDescription()
-//   {
-//     return "gis|controller." + this.name;
-//   }
-// }
+      application.addObject(this.object, application.baseObject);
+      application.notifyObjectsChanged(camera, this);
 
-// Controller.addClass(WMSController);
+      console.log(`[WMSController] WMS layer '${this.layers}' created.`);
+    } 
+    catch (err)
+    {
+      console.error("Error updating WMS map:", err);
+    }
+  }
 
-// export { WMSController };
+  removeMap()
+  {
+    if (this._mapView) {
+        if (this._mapView.dispose)
+        {
+          this._mapView.dispose();
+        }
+        this._mapView.removeFromParent();
+        this._mapView = null;
+        this.application.notifyObjectsChanged(this.object, this, "structureChanged");
+    }
+  }
+
+  static getDescription()
+  {
+    return "gis|controller.WMSController";
+  }
+}
+
+Controller.addClass(WMSController);
+
+export { WMSController };
--- bimrocket-webapp/src/main/webapp/js/tools/WmsImportTool.js
+++ bimrocket-webapp/src/main/webapp/js/tools/WmsImportTool.js
@@ -1,55 +1,59 @@
+/**
+ * WmsImportTool.js
+ *
+ * @author nexus
+ */
+
 import { Tool } from "./Tool.js";
 import { Dialog } from "../ui/Dialog.js";
-import { WMSLoader } from "../io/gis/WMSLoader.js";
+import { WMSController } from "../controllers/WMSController.js";
 import { MessageDialog } from "../ui/MessageDialog.js";
 import * as THREE from "three";
-import { MapView } from "geo-three";
-import proj4 from 'proj4';
-
-// Projecció Web Mercator (EPSG:3857)
-proj4.defs("EPSG:3857", "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs");
-
-// Projecció UTM zona 31N (EPSG:25831)
-proj4.defs("EPSG:25831", "+proj=utm +zone=31 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs");
 
 class WmsImportTool extends Tool
 {
-    constructor(application)
+    constructor(application, options)
     {
         super(application);
-        this.name = "wms_import";
+        this.name = "wmsImport";
         this.label = "tool.wms_import.label";
         this.help = "tool.wms_import.help";
         this.className = "wmsImport";
-        this.immediate = true;
+        this.immediate = false;
         
         // Move wmsConfigs initialization before dialog creation
-        this.wmsConfigs = {
-            "icgc_orto_div_proxy": {
+        this.wmsConfigs = 
+        {
+            "icgc_orto_div_proxy":
+            {
                 label: "ICGC - Divisions + Orto",
                 url: "https://geoserver.nexusgeografics.com/geoserver/bimrocket/wms",
                 layer: "icgc_orto_divisions",
                 crs: "EPSG:3857"
             },
-            "icgc_topo": {
+            "icgc_topo":
+            {
                 label: "ICGC - Topogràfic gris",
                 url: "https://geoserveis.icgc.cat/icc_mapesmultibase/noutm/wms/service",
                 layer: "topogris",
                 crs: "EPSG:3857"
             },
-            "icgc_orto": {
+            "icgc_orto":
+            {
                 label: "ICGC - Ortofoto",
                 url: "https://geoserveis.icgc.cat/icc_mapesmultibase/noutm/wms/service",
                 layer: "orto",
                 crs: "EPSG:3857"
             },
-            "icgc_geologic": {
+            "icgc_geologic":
+            {
                 label: "ICGC - Divisions administratives 3857",
                 url: "https://geoserveis.icgc.cat/servei/catalunya/divisions-administratives/wms/service",
                 layer: "divisions_administratives_capsdemunicipi_capcomarca,divisions_administratives_capsdemunicipi_capmunicipi,divisions_administratives_municipis_5000,divisions_administratives_municipis_50000,divisions_administratives_municipis_100000,divisions_administratives_municipis_250000,divisions_administratives_comarques_5000,divisions_administratives_comarques_50000,divisions_administratives_comarques_100000,divisions_administratives_comarques_250000,divisions_administratives_comarques_500000,divisions_administratives_comarques_1000000",
                 crs: "EPSG:3857"
             },
-            "icgc_geologic2": {
+            "icgc_geologic2":
+            {
                 label: "ICGC - Divisions administratives 25831",
                 url: "https://geoserveis.icgc.cat/servei/catalunya/divisions-administratives/wms/service",
                 layer: "divisions_administratives_capsdemunicipi_capcomarca,divisions_administratives_capsdemunicipi_capmunicipi,divisions_administratives_municipis_5000,divisions_administratives_municipis_50000,divisions_administratives_municipis_100000,divisions_administratives_municipis_250000,divisions_administratives_comarques_5000,divisions_administratives_comarques_50000,divisions_administratives_comarques_100000,divisions_administratives_comarques_250000,divisions_administratives_comarques_500000,divisions_administratives_comarques_1000000",
@@ -58,26 +62,20 @@ class WmsImportTool extends Tool
         };
         
         this.dialog = this.createDialog();
-        this.wmsLayerGroup = null;
+        this.setOptions(options);
+        application.addTool(this);
     }
 
-    execute()
-    {
-        this.dialog.show();
+    activate()
+    { 
+        super.activate();
+        this.dialog.show(); 
     }
 
-    cleanup()
-    {
-        if (this.wmsLayerGroup)
-        {
-            if (this.wmsLayerGroup.parent)
-            {
-                const mapView = this.wmsLayerGroup.getObjectByProperty('isMapView', true);
-                if (mapView) { mapView.dispose(); }
-                this.application.removeObject(this.wmsLayerGroup, null, true);
-            }
-            this.wmsLayerGroup = null;
-        }
+    deactivate()
+    { 
+        super.deactivate();
+        this.dialog.hide();
     }
 
     createDialog()
@@ -86,7 +84,8 @@ class WmsImportTool extends Tool
         dialog.setSize(400, 350);
         dialog.setI18N(this.application.i18n);
 
-        const createLabeledInput = (labelText) => {
+        const createLabeledInput = (labelText) => 
+        {
             const container = document.createElement("div");
             container.style.marginTop = "10px";
             
@@ -99,7 +98,6 @@ class WmsImportTool extends Tool
             input.type = "text";
             input.style.width = "95%";
             input.style.padding = "8px";
-            //input.readOnly = true;
             
             container.appendChild(label);
             container.appendChild(input);
@@ -109,7 +107,7 @@ class WmsImportTool extends Tool
         // Create select for WMS configurations
         const selectContainer = document.createElement("div");
         selectContainer.style.marginTop = "10px";
-        
+
         const selectLabel = document.createElement("label");
         selectLabel.textContent = "Selecciona configuració WMS:";
         selectLabel.style.display = "block";
@@ -133,7 +131,8 @@ class WmsImportTool extends Tool
         this.crsInput = crsInput;
 
         // Add options to select
-        Object.entries(this.wmsConfigs).forEach(([key, config]) => {
+        Object.entries(this.wmsConfigs).forEach(([key, config]) =>
+        {
             const option = document.createElement("option");
             option.value = key;
             option.text = config.label;
@@ -141,7 +140,8 @@ class WmsImportTool extends Tool
         });
 
         // Add change event listener
-        configSelect.addEventListener("change", () => {
+        configSelect.addEventListener("change", () =>
+        {
             const config = this.wmsConfigs[configSelect.value];
             urlInput.value = config.url;
             layersInput.value = config.layer;
@@ -156,104 +156,50 @@ class WmsImportTool extends Tool
         // Trigger initial load
         configSelect.dispatchEvent(new Event("change"));
 
-        dialog.addButton("import", "button.accept", () => this.importWmsUrl());
+        dialog.addButton("import", "button.accept", () => this.addWMS());
         dialog.addButton("close", "button.close", () => this.closeDialog());
         
         return dialog;
     }
 
-    importWmsUrl()
+    addWMS()
     {
-        console.log("--- [DEBUG] Iniciant importació WMS ---");
-        //TODO: descomentar per esborrar capa anterior i que no xoquin
-        // this.cleanup();
-
         const url = this.urlInput.value;
         const layers = this.layersInput.value;
         const crs = this.crsInput.value;
+        const application = this.application;
 
-        if (!url || !layers || !crs)
-        {
-            MessageDialog.create("ERROR", "URL, capa i CRS són obligatoris.").show();
+        //TODO: traduccions
+        if (!url || !layers || !crs) {
+            MessageDialog.create("ERROR", "URL, capa i CRS són obligatoris.").setI18N(application.i18n).show();
             return;
         }
 
-        try
+        const wmsLayerGroup = new THREE.Group();
+        wmsLayerGroup.name = "WMS Layer - " + layers;
+
+        if(this.wmsLayerGroup)
         {
-            const application = this.application;
-            const camera = application.camera;
-            const provider = new WMSLoader(url, layers, crs);
-            const renderer = new THREE.WebGLRenderer();
-            const mapViewGeoT = new MapView(MapView.PLANAR, provider, camera);
+            this.application.removeObject(this.wmsLayerGroup);
+            this.wmsLayerGroup = null;
+        }
 
-            provider.minZoom = 13;
-            camera.position.z += 0.00001;
-            mapViewGeoT.name = "MapView";
-            mapViewGeoT.subDivisionsRays = 64;
-            renderer.render(application.scene, camera);
-
-            function animate() 
-            {
-                requestAnimationFrame(animate);
-                mapViewGeoT.updateMatrixWorld(camera);
-                renderer.render(application.scene, camera);
-            }
-            
-            animate();
-
-            this.wmsLayerGroup = new THREE.Group();
-            this.wmsLayerGroup.name = "WMS Layer - " + layers;
-            this.wmsLayerGroup.add(mapViewGeoT);
-
-            this.wmsLayerGroup.rotation.x = Math.PI/2;
-            this.wmsLayerGroup.rotation.y = 0;
-
-            // TODO - Fer-ho dinàmic.
-            this.wmsLayerGroup.position.set
-            (
-                253,
-                5668,
-                -1.5
-            );
-
-            this.wmsLayerGroup.updateMatrix();
-            this.wmsLayerGroup.updateMatrixWorld(true);
-
-            application.addObject(this.wmsLayerGroup, application.baseObject);
-            application.notifyObjectsChanged(camera, this);
-
-            this.closeDialog();
-
-            console.log("--- [DEBUG] Importació WMS finalitzada. ---");
-
-        } catch (err)
+        if (!wmsLayerGroup.controllers)
         {
-            console.error("Error durant la importació WMS:", err);
-            MessageDialog.create("ERROR", "Error durant la importació: " + err.message)
-              .setClassName("error")
-              .setI18N(this.application.i18n).show();
-        }
-    }
-    
-    /**
-     * Mètode de neteja. Elimina la capa i els listeners.
-     */
-    cleanup() {
-        if (this.updateListener) {
-            this.application.removeEventListener("animation", this.updateListener);
-            this.application.removeEventListener("camera-change-end", this.updateListener);
-            this.updateListener = null;
+            wmsLayerGroup.controllers = {};
         }
 
-        if (this.wheelListener) {
-            this.application.renderer.domElement.removeEventListener('wheel', this.wheelListener);
-            this.wheelListener = null;
-        }
+        const controller = new WMSController(wmsLayerGroup, "wms_controller");
+        controller.url = url;
+        controller.layers = layers;
+        controller.crs = crs;
         
-        if (this.wmsLayer) {
-            this.application.removeObject(this.wmsLayer);
-            this.wmsLayer = null;
-        }
+        wmsLayerGroup.controllers["wms_controller"] = controller;
+        
+        application.addObject(this.wmsLayerGroup, application.baseObject);
+        application.initControllers(wmsLayerGroup);
+                
+        this.closeDialog();
     }
 
     closeDialog()
