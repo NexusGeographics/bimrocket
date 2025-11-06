8517d2d8c0c69d9e67fe30e4942d3fbe9fe9d659
aagusti <aagusti@nexusgeographics.com>
2025-11-06 15:15:24 +0100

    feat: change proxy valid url's and environment server url

--- bimrocket-webapp/src/main/webapp/js/Environment.js
+++ bimrocket-webapp/src/main/webapp/js/Environment.js
@@ -2,9 +2,7 @@
 
 export const Environment =
 {
-  // TODO: canviar server_url!
-  // SERVER_URL : "/bimrocket-server",
-  SERVER_URL : "https://localhost:9090/bimrocket-server",
+  SERVER_URL : "/bimrocket-server",
   SERVER_ALIAS : "bimrocket",
   MODULES : ["base", "bim", "gis"]
 };
